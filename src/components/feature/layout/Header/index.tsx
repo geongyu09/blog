0b759d406d56397422a3e3044f250864bd55da94
박건규 <geongyu09@gmail.com>
2024-08-28 19:52:15 +0900

    refactor: header 컴포넌트 리팩토링
    불필요한 div 사용 제거
    
    #3

--- src/components/feature/layout/Header/index.tsx
+++ src/components/feature/layout/Header/index.tsx
@@ -1,24 +1,19 @@
-import Container from '@/components/common/layout/Container';
 import Link from 'next/link';
 
 export default function Header() {
   return (
-    <header className="">
-      <Container>
-        <div className="py-4 flex justify-between">
-          <h1 className="font-bold text-2xl">Geongyu</h1>
-          <nav className="text-xl">
-            <ul className="flex gap-4">
-              <li>
-                <Link href="/">Home</Link>
-              </li>
-              <li>
-                <Link href="/about">About</Link>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </Container>
+    <header className="px-8 py-4 flex justify-between">
+      <h1 className="font-bold text-2xl">Geongyu</h1>
+      <nav className="text-xl">
+        <ul className="flex gap-4">
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
