4797346570b1d5b48a2ba0486d543449c1edd187
박건규 <geongyu09@gmail.com>
2024-09-12 17:21:10 +0900

    feat: 링크 hover 스타일 추가
    #7

--- src/components/feature/layout/Header/index.tsx
+++ src/components/feature/layout/Header/index.tsx
@@ -7,14 +7,22 @@ export default function Header() {
     <header className="sticky top-0 bg-white z-50">
       <Container>
         <div className="py-4 flex justify-between">
-          <h1 className="font-bold text-2xl">Geongyu</h1>
+          <h1 className="font-bold text-2xl">
+            <Link href="/" className="link-hover">
+              Geongyu
+            </Link>
+          </h1>
           <nav className="text-xl">
             <ul className="flex gap-4">
               <li>
-                <Link href="/">Home</Link>
+                <Link href="/" className="link-hover">
+                  Home
+                </Link>
               </li>
               <li>
-                <Link href="/about">About</Link>
+                <Link href="/about" className="link-hover">
+                  About
+                </Link>
               </li>
               <li>
                 <Link href="https://github.com/geongyu09" target="_blank">
