72d7b63c032322b4527e0e5615603a9cc594ee66
박건규 <geongyu09@gmail.com>
2024-09-12 21:34:33 +0900

    feat: HomeBanner 추가
    #6

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,8 @@ import Container from '@/components/common/layout/Container';
 import Gap from '@/components/common/layout/Gap';
 import SplitLayout from '@/components/common/layout/SplitLayout';
 import Tag from '@/components/common/tag';
-
-import Banner from '@/components/feature/Home/HomeBanner';
-import MonitoringEmbedSection from '@/components/feature/Home/MonitoringEmbedSection';
+import HomeBanner from '@/components/feature/Home/HomeBanner';
+// import MonitoringEmbedSection from '@/components/feature/Home/MonitoringEmbedSection';
 import Posts from '@/components/feature/Home/Posts';
 import SideBarMenu from '@/components/feature/Home/SideBarMenu';
 import Link from 'next/link';
@@ -15,19 +14,9 @@ interface HomeProps {
 export default function Home({ searchParams: { tag } }: HomeProps) {
   return (
     <>
-      <Gap size={12} />
-      {/* <Container>
-        <div className="text-center h-80 flex flex-col justify-center">
-          <h1 className="text-4xl font-bold">"Hello World!" 👋</h1>
-          <Gap size={4} />
-          <p className="text-lg">안녕하세요! 00하는 개발자 박건규입니다!</p>
-        </div>
-      </Container> */}
-
-      <Banner />
+      <HomeBanner />
       <Gap size={12} />
       <Container>
-        {/* <MonitoringEmbedSection /> */}
         <Gap size={12} />
 
         <h2 className="text-2xl font-bold">전체 포스트</h2>
