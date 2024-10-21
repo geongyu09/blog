a7c3566183380969172a075ae2e7faadd65a8c05
박건규 <geongyu09@gmail.com>
2024-10-21 13:39:51 +0900

    feat: 사이드바 애니매이션 추가

--- src/components/feature/Post/SideTableOfContent/index.tsx
+++ src/components/feature/Post/SideTableOfContent/index.tsx
@@ -2,32 +2,26 @@
 
 import MarkdownNav from '@/components/common/lib/MarkdownNav';
 import ShortTableOfContent from '@/components/common/lib/ShortTableOfContent';
-import { useState } from 'react';
 
 interface SideTableOfContentProps {
   content: string;
 }
+
 export default function SideTableOfContent({
   content,
 }: SideTableOfContentProps) {
-  const [isHover, setIsHover] = useState(false);
-
   return (
-    <div
-      className="fixed top-64 right-0 h-fit flex flex-col items-end gap-2 pr-2"
-      onMouseEnter={() => setIsHover(true)}
-      onMouseLeave={() => setIsHover(false)}
-    >
-      <ShortTableOfContent content={content} />
+    <div className="fixed top-64 right-0 h-fit flex flex-col items-end gap-2 pr-2 group">
+      <div className="transition-all duration-300 ease-in-out opacity-100 group-hover:opacity-0">
+        <ShortTableOfContent content={content} />
+      </div>
 
-      {isHover && (
-        <div className="fixed top-60 right-0 h-fit flex flex-col gap-2 pr-2">
-          <MarkdownNav
-            markdown={content}
-            className="bg-white rounded-xl border p-4 shadow-md"
-          />
-        </div>
-      )}
+      <div className="transition-all duration-180 ease-in-out fixed top-60 h-fit opacity-0 group-hover:opacity-100 ">
+        <MarkdownNav
+          markdown={content}
+          className="bg-white rounded-xl border p-4 shadow-md"
+        />
+      </div>
     </div>
   );
 }
