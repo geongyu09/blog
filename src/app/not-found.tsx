7556b4de7d68114038dfaa1876d3722c0f4b4d6f
박건규 <geongyu09@gmail.com>
2024-10-21 20:55:55 +0900

    feat: not found 페이지 추가
    #13

---
+++ src/app/not-found.tsx
@@ -0,0 +1,30 @@
+import Link from 'next/link';
+
+import Gap from '@/components/common/layout/Gap';
+import Image from 'next/image';
+
+export default function NotFound() {
+  return (
+    <section className="flex flex-col items-center select-none">
+      <Gap size={5} />
+      <Image
+        src="/assets/404/codingCat.png"
+        alt="404"
+        width={900}
+        height={300}
+      />
+      <Gap size={10} />
+      <h2 className="text-4xl font-semibold">404 - Not Found</h2>
+      <Gap size={1} />
+      <p>
+        찾고계신 페이지가 없습니다! 혹은 해당 페이지는 열심히 준비중이니 조금만
+        기다려주세요 🙃
+      </p>
+      <Gap size={5} />
+
+      <Link className="bg-indigo-500 text-white px-4 py-2 rounded-lg" href="/">
+        홈으로 가기
+      </Link>
+    </section>
+  );
+}
