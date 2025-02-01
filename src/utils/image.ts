08d76c598c2f4d338bca016c6088245987bc2471
박건규 <geongyu09@gmail.com>
2025-02-01 21:56:12 +0900

    fix: 개발 모드에서도 올바르게 이미지를 불러올 수 있도록 이미지 주소를 가져오는 getImagePath함수 수정
    #5

--- src/utils/image.ts
+++ src/utils/image.ts
@@ -1,5 +1,9 @@
 import BASE_IMAGE_PATH from '@/constants/path/ImagePath';
 
-export const getImagePath = (path: string) => `${BASE_IMAGE_PATH}/${path}`;
+export const getImagePath = (path: string) => {
+  return process.env.NODE_ENV === 'development'
+    ? path
+    : `${BASE_IMAGE_PATH}/${path}`;
+};
 
 export default Object.freeze({ getImagePath });
