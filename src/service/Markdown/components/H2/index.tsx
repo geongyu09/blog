4a814ef5be65624e8493ac5b358230430fa823b9
박건규 <geongyu09@gmail.com>
2025-02-01 23:12:49 +0900

    fix: 올바르게 head scroll이 되지 않았던 버그 수정
    기존에 인코딩으로 인해서 올바르게 head scroll이 안되던 버그를 디코딩 하여 수정
    추가적으로 fixed 형태인 header 컴포넌트에 가려서 스크롤이 되어도 header 컴포넌트에 가려 보이지 안던 현상을 scroll margin을 주어 해결

--- src/service/Markdown/components/H2/index.tsx
+++ src/service/Markdown/components/H2/index.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
 import cn from '@/utils/cn';
 import headerUtil from '@/utils/contentHeader';
 import Link from 'next/link';
+import React from 'react';
 
 interface H2Props extends React.HTMLAttributes<HTMLHeadingElement> {
   children?: React.ReactNode;
@@ -13,13 +13,15 @@ export default function H2({
   className = '',
   ...rest
 }: H2Props): JSX.Element {
-  const headerId = headerUtil.getHeaderHashText(children as string);
+  const headerId = decodeURIComponent(
+    headerUtil.getHeaderHashText(children as string),
+  );
   return (
     <Link href={`#${headerId}`}>
       <h5
         id={headerId}
         className={cn(
-          'text-2xl font-bold mt-12 mb-8',
+          'text-2xl font-bold mt-12 mb-8 scroll-mt-20',
           'hover:opacity-80',
           className,
         )}
