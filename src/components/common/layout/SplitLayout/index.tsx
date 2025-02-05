8a6c6fc01eb871f6cd42e29d6e4bdaf334dd8ea6
박건규 <geongyu09@gmail.com>
2025-02-05 23:31:24 +0900

    feat: 반응형 뷰를 위하여 SplitLayout 컴포넌트의 cva에 responsive, responsiveGap 속성을 추가
    반응형으로 해당 컴포넌트를 사용할 수 있도록 cva 속성 추가
    
    #22

--- src/components/common/layout/SplitLayout/index.tsx
+++ src/components/common/layout/SplitLayout/index.tsx
@@ -1,6 +1,6 @@
-import { PropsWithChildren } from 'react';
-import { cva, VariantProps } from 'class-variance-authority';
 import cn from '@/utils/cn';
+import { cva, VariantProps } from 'class-variance-authority';
+import { PropsWithChildren } from 'react';
 
 const SplitLayoutVariants = cva(`grid`, {
   variants: {
@@ -12,6 +12,17 @@ const SplitLayoutVariants = cva(`grid`, {
       lg: 'gap-16',
       xl: 'gap-28',
     },
+    responsive: {
+      none: '',
+      default: 'grid-cols-1 lg:grid-cols-[10fr,5fr]',
+      reverse: 'grid-cols-1 lg:grid-cols-[10fr,5fr]',
+    },
+    responsiveGap: {
+      none: '',
+      md: 'gap-0 lg:gap-4',
+      lg: 'gap-8 lg:gap-16',
+      xl: 'gap-12 lg:gap-28',
+    },
   },
 });
 
@@ -19,17 +30,34 @@ interface SplitLayoutProps
   extends PropsWithChildren,
     VariantProps<typeof SplitLayoutVariants> {
   sidebar: React.ReactNode;
+  styles?: string;
 }
 export default function SplitLayout({
   children,
   sidebar,
   gridCols = 'default',
   gap = 'md',
+  styles,
+  responsive,
+  responsiveGap,
 }: SplitLayoutProps) {
   return (
-    <div className={cn(SplitLayoutVariants({ gridCols, gap }))}>
-      <div>{children}</div>
-      <div>{sidebar}</div>
+    <div
+      className={cn(
+        SplitLayoutVariants({ gridCols, gap, responsive, responsiveGap }),
+        styles,
+      )}
+    >
+      <div className={responsive === 'reverse' ? 'order-2 lg:order-none' : ''}>
+        {children}
+      </div>
+      <div className={responsive === 'reverse' ? 'order-1 lg:order-none' : ''}>
+        {sidebar}
+      </div>
     </div>
   );
 }
+
+SplitLayout.defaultProps = {
+  styles: '',
+};
