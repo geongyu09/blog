57af242c8108b240a935704faa1e7af1d12e6d5e
박건규 <geongyu09@gmail.com>
2025-02-05 23:32:02 +0900

    feat: 반응형 뷰를 위하여 container 컴포넌트의 cva에 responsive 속성 추가
    #22

--- src/components/common/layout/Container/index.tsx
+++ src/components/common/layout/Container/index.tsx
@@ -1,5 +1,26 @@
+import cn from '@/utils/cn';
+import { cva, VariantProps } from 'class-variance-authority';
 import { PropsWithChildren } from 'react';
 
-export default function Container({ children }: PropsWithChildren) {
-  return <div className="mx-auto max-w-6xl">{children}</div>;
+const ContainerVariants = cva('mx-auto max-w-6xl', {
+  variants: {
+    responsive: {
+      none: '',
+      default: 'px-4 lg:px-0',
+    },
+  },
+});
+
+interface ContainerProps
+  extends PropsWithChildren,
+    VariantProps<typeof ContainerVariants> {}
+
+export default function Container({ responsive, children }: ContainerProps) {
+  return (
+    <div className={cn(ContainerVariants({ responsive }))}>{children}</div>
+  );
 }
+
+Container.defaultProps = {
+  styles: '',
+};
