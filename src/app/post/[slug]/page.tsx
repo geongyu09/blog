ab41ff7001dfa84821249cfe26fe195185a0df9c
박건규 <geongyu09@gmail.com>
2025-02-06 17:43:55 +0900

    feat: detail 페이지에 반응형 추가
    #5

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,6 +1,5 @@
 import Container from '@/components/common/layout/Container';
 import Gap from '@/components/common/layout/Gap';
-import Wrapper from '@/components/common/layout/Wrapper';
 import MarkdownNav from '@/components/common/lib/MarkdownNav';
 import SideTableOfContent from '@/components/feature/Post/SideTableOfContent';
 import { getPostBySlug, getPostSlugs } from '@/lib/post/post';
@@ -19,33 +18,31 @@ export default function Page({ params: { slug } }: PageProps) {
   } = getPostBySlug(slug);
 
   return (
-    <Container>
+    <Container width="content" responsive="default">
       <Gap size={4} />
 
-      <Wrapper padding={48}>
-        <Gap size={12} />
-        <h2 className="text-4xl font-bold">{title}</h2>
-        <Gap size={4} />
-        <p className="text-lg font-semibold opacity-90">{description}</p>
-        <Gap size={2} />
-        <span className="text-lg font-semibold opacity-70">
-          {/* TODO: 업로드 형태 정하기 혹은 해당 로직을 함수로 빼기 */}
-          업로드 날짜: {date.split('T')[0]}
-        </span>
-        <Gap size={4} />
-        <hr />
+      <Gap size={12} />
+      <h2 className="text-4xl font-bold">{title}</h2>
+      <Gap size={4} />
+      <p className="text-lg font-semibold opacity-90">{description}</p>
+      <Gap size={2} />
+      <span className="text-lg font-semibold opacity-70">
+        {/* TODO: 업로드 형태 정하기 혹은 해당 로직을 함수로 빼기 */}
+        업로드 날짜: {date.split('T')[0]}
+      </span>
+      <Gap size={4} />
+      <hr />
 
-        <Gap size={4} />
-        <MarkdownNav markdown={content} />
-        <hr />
-        <MarkdownViewer markdown={content} />
+      <Gap size={4} />
+      <MarkdownNav markdown={content} />
+      <hr />
+      <MarkdownViewer markdown={content} />
 
-        <Gap size={12} />
-        <hr />
-        <Gap size={12} />
+      <Gap size={12} />
+      <hr />
+      <Gap size={12} />
 
-        <Comment />
-      </Wrapper>
+      <Comment />
       <SideTableOfContent content={content} />
     </Container>
   );
