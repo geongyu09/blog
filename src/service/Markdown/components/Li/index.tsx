6b83c5f16b9ece0b788c60c8ce01eb47db1bea5c
박건규 <geongyu09@gmail.com>
2025-02-06 20:02:23 +0900

    design: 블로그 포스트의 header의 크기가 잘 구분되지 않아 가독성이 떨어지던 부분 개선
    기존에 h2와 h3가 크기가 동일하여 문단 구분이 어려웠다는 피드백을 받고,
    크기 및 간격 조정
    
    #5

--- src/service/Markdown/components/Li/index.tsx
+++ src/service/Markdown/components/Li/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
 import cn from '@/utils/cn';
+import React from 'react';
 
 interface LiProps extends React.HTMLAttributes<HTMLLIElement> {
   children?: React.ReactNode;
@@ -12,7 +12,10 @@ export default function Li({
   ...rest
 }: LiProps): JSX.Element {
   return (
-    <li className={cn('text-lg my-2', className)} {...rest}>
+    <li
+      className={cn('text-lg my-2 ml-5 marker:m-0 marker:p-0', className)}
+      {...rest}
+    >
       {children}
     </li>
   );
