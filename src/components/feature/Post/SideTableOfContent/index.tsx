a4b8097df0572d5bca9f0d000afa584ca668083b
박건규 <geongyu09@gmail.com>
2025-02-06 18:03:10 +0900

    refactor: SideTableOfContent를 재사용 가능한 컴포넌트드로 리팩토링한 컴포넌트를 사용하여 재 구성
    #15

--- src/components/feature/Post/SideTableOfContent/index.tsx
+++ src/components/feature/Post/SideTableOfContent/index.tsx
@@ -1,23 +1,16 @@
 'use client';
 
+import HoverToShow from '@/components/common/interaction/HoverToShow';
+import SideFixedPositioner, {
+  SideFixedPositionerVariants,
+} from '@/components/common/layout/SideFixedPositioner';
 import MarkdownNav from '@/components/common/lib/MarkdownNav';
 import ShortTableOfContent from '@/components/common/lib/ShortTableOfContent';
-import cn from '@/utils/cn';
-import { cva, VariantProps } from 'class-variance-authority';
+import { VariantProps } from 'class-variance-authority';
 
-const SideTabOfContentVariants = cva('fixed top-64 right-0 w-64', {
-  variants: {
-    responsive: {
-      none: '',
-      default: 'hidden lg:block',
-      hide: 'hidden lg:block',
-    },
-  },
-});
-
-interface SideTableOfContentProps
-  extends VariantProps<typeof SideTabOfContentVariants> {
+interface SideTableOfContentProps {
   content: string;
+  responsive?: VariantProps<typeof SideFixedPositionerVariants>['responsive'];
 }
 
 export default function SideTableOfContent({
@@ -25,23 +18,20 @@ export default function SideTableOfContent({
   responsive = 'default',
 }: SideTableOfContentProps) {
   return (
-    <div className={cn(SideTabOfContentVariants({ responsive }))}>
-      <div className="relative">
-        {/* Short TOC */}
-        <div className="relative group">
-          <div className="transition-all duration-300 ease-in-out">
-            <ShortTableOfContent content={content} />
-          </div>
-
-          {/* Full TOC */}
-          <div className="absolute top-0 right-0 w-full transition-all duration-300 ease-in-out opacity-0 invisible group-hover:opacity-100 group-hover:visible">
-            <MarkdownNav
-              markdown={content}
-              className="bg-white rounded-xl border p-4 shadow-md max-h-96 overflow-y-auto scrollbar-hidden overscroll-contain"
-            />
-          </div>
-        </div>
-      </div>
-    </div>
+    <SideFixedPositioner responsive={responsive}>
+      <HoverToShow
+        nonHoverComponent={<ShortTableOfContent content={content} />}
+        hoverComponent={
+          <MarkdownNav
+            markdown={content}
+            className="bg-white rounded-xl border p-4 shadow-md max-h-96 overflow-y-auto scrollbar-hidden overscroll-contain"
+          />
+        }
+      />
+    </SideFixedPositioner>
   );
 }
+
+SideTableOfContent.defaultProps = {
+  responsive: 'default',
+};
