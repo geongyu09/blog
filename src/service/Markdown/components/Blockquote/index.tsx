0435599969f69b5112ae8822a17d3be413ae3b5f
박건규 <geongyu09@gmail.com>
2025-02-08 22:07:27 +0900

    feat: md의 blockquote 태그 레이아웃 및 스타일 변경
    #5

--- src/service/Markdown/components/Blockquote/index.tsx
+++ src/service/Markdown/components/Blockquote/index.tsx
@@ -1,15 +1,16 @@
-import React from 'react';
 import cn from '@/utils/cn';
-import './styled.css';
+import React from 'react';
+
+import './styled.css'; // 적용이 안됨
 
 export type QuoteStyle = 'default' | 'info' | 'warning' | 'success' | 'error';
 
 const styleClasses: Record<QuoteStyle, string> = {
-  default: 'border-gray-300 bg-slate-100',
-  info: 'border-blue-300 bg-blue-50',
-  warning: 'border-yellow-300 bg-yellow-50',
-  success: 'border-green-300 bg-green-50',
-  error: 'border-red-300 bg-red-50',
+  default: 'border-gray-300 bg-slate-100 px-8',
+  info: 'border-blue-300 bg-blue-50 flex px-8 gap-8 items-center',
+  warning: 'border-yellow-300 bg-yellow-50 flex px-8 gap-8 items-center',
+  success: 'border-green-300 bg-green-50 flex px-8 gap-8 items-center',
+  error: 'border-red-300 bg-red-50 flex px-8 gap-8 items-center',
 };
 
 interface BlockquoteProps extends React.HTMLAttributes<HTMLQuoteElement> {
@@ -26,45 +27,52 @@ export default function Blockquote({
 }: BlockquoteProps): JSX.Element {
   if (!node) return <div />;
 
-  const quoteStyleType = (node.children[1].children[0] as any).value?.split(
-    '\n',
-  )[0];
+  const contents = (node.children[1].children[0] as any).value?.split('\n');
+
   let styleType: QuoteStyle = 'default';
 
-  if (quoteStyleType === '🍀') styleType = 'success';
-  if (quoteStyleType === '⚠️' || quoteStyleType === '❗️') styleType = 'warning';
-  if (quoteStyleType === '❌') styleType = 'error';
-  if (quoteStyleType === '💡') styleType = 'info';
+  if (!contents) styleType = 'default';
+  if (contents?.[0] === '🍀') styleType = 'success';
+  if (contents?.[0] === '⚠️' || contents?.[0] === '❗️') styleType = 'warning';
+  if (contents?.[0] === '❌') styleType = 'error';
+  if (contents?.[0] === '💡') styleType = 'info';
 
   return (
     <blockquote
       className={cn(
-        'border-l-4 border-gray-300 my-12 bg-slate-100 px-8 py-2 rounded-md whitespace-pre-wrap',
-        'flex gap-8 items-center',
+        'border-l-4 border-gray-300 my-12 bg-slate-100 py-2 rounded-md whitespace-pre-wrap',
         'blockquote',
+        styleClasses[styleType],
         className,
-        { [styleClasses[styleType]]: styleType !== 'default' },
       )}
       {...rest}
     >
-      {styleType !== 'default' &&
-        children &&
-        children instanceof Array &&
+      {children instanceof Array &&
         children.map((childrenText, index) => {
           const isEmptyString = childrenText === '\n' || childrenText === '';
 
-          if (!isEmptyString)
-            return (
+          return (
+            !isEmptyString && (
               <div
                 key={`${index * 2}-${childrenText}`}
-                className="first:text-2xl"
+                className="flex items-center"
               >
-                {childrenText}
+                {styleType !== 'default' && index === 1 ? (
+                  <div className="mark">{childrenText}</div>
+                ) : (
+                  <div
+                    key={`${index * 2}-${childrenText}`}
+                    className={cn('text-lg', {
+                      'text-xl': index === 0,
+                    })}
+                  >
+                    {childrenText}
+                  </div>
+                )}
               </div>
-            );
-          return null;
+            )
+          );
         })}
-      {styleType === 'default' && children}
     </blockquote>
   );
 }
