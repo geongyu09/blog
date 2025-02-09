9a86d19bd19a962041141ccc6b5369d50692a140
박건규 <geongyu09@gmail.com>
2025-02-09 22:40:16 +0900

    feat: 에러 페이지 추가

---
+++ src/app/error.tsx
@@ -0,0 +1,55 @@
+'use client';
+
+import Gap from '@/components/common/layout/Gap';
+import ROUTE_PATH from '@/constants/path/routePath';
+import Link from 'next/link';
+import { useEffect } from 'react';
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  const codingCat = '/assets/error/Designer.jpeg';
+
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
+  return (
+    <section className="flex flex-col items-center select-none h-fit-to-screen">
+      <Gap size={5} />
+      <div
+        style={{
+          width: '900px',
+          height: '350px',
+          backgroundImage: `url(${codingCat})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+        }}
+        className="bg-cover rounded-xl"
+      />
+      <Gap size={10} />
+      <h2 className="text-4xl font-semibold">아니 이럴수가..</h2>
+      <Gap size={8} />
+      <p className="text-lg text-center">
+        에러가 발생했습니다 🤦‍♂️ 다시 한번 시도해주세요! 혹은 홈으로 가서 다른
+        페이지를 확인해보세요. 🙇‍♂️
+      </p>
+      <Gap size={12} />
+
+      <button
+        type="button"
+        className="bg-indigo-500 text-white px-4 py-2 rounded-lg"
+        onClick={reset}
+      >
+        다시 시도하기
+      </button>
+
+      <Gap size={4} />
+      <Link href={ROUTE_PATH.HOME}>홈으로 가기</Link>
+    </section>
+  );
+}
