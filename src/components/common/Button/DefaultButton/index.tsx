aa3b67ddca88b4e48065d9a06b9190b5197c7e58
박건규 <geongyu09@gmail.com>
2025-02-10 23:52:04 +0900

    refactor: DefaultButton 공통 컴포넌트를 재사용가능하도록 cva 속성 정의
    #22

--- src/components/common/Button/DefaultButton/index.tsx
+++ src/components/common/Button/DefaultButton/index.tsx
@@ -1,18 +1,42 @@
 import cn from '@/utils/cn';
+import { cva, VariantProps } from 'class-variance-authority';
 
-interface DefaultButtonProps {
+const DefaultButtonVariants = cva(
+  `py-4 text-center block bg-slate-100 rounded-md text-slate-900 font-semibold hover:bg-slate-200 transition-all`,
+  {
+    variants: {
+      theme: {
+        default: 'bg-gray-500 text-white hover:bg-slate-200',
+        primary: 'bg-indigo-500 text-white hover:bg-indigo-400',
+        secondary: 'bg-gray-500 text-white hover:bg-slate-200',
+      },
+      widthStyle: {
+        default: 'w-full',
+        auto: 'w-auto px-4 py-2 rounded-lg w-fit',
+      },
+    },
+  },
+);
+
+interface DefaultButtonProps
+  extends React.HTMLProps<HTMLButtonElement>,
+    VariantProps<typeof DefaultButtonVariants> {
   classname?: string;
   text?: string;
 }
 
-export default function DefaultButton({ text, classname }: DefaultButtonProps) {
+export default function DefaultButton({
+  text,
+  classname,
+  onClick,
+  theme = 'default',
+  widthStyle = 'default',
+}: DefaultButtonProps) {
   return (
     <button
       type="button"
-      className={cn(
-        'w-full py-4 text-center block bg-slate-100 rounded-md text-slate-900 font-semibold hover:bg-slate-200 transition-all',
-        classname,
-      )}
+      className={cn(DefaultButtonVariants({ theme, widthStyle }), classname)}
+      onClick={onClick}
     >
       {text}
     </button>
